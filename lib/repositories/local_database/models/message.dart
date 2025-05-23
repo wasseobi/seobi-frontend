9fb4fb0bb597a8a0bbe2f8d8c7ea818f6c96a4f4
zer0ken <a3592777@gmail.com>
2025-05-24 04:39:08 +0900

    ✅ 로컬 DB 수정 후 테스트

--- lib/repositories/local_database/models/message.dart
+++ lib/repositories/local_database/models/message.dart
@@ -3,7 +3,6 @@ import 'message_role.dart';
 class Message {
   final String id;
   final String sessionId;
-  final String userId;
   final String? content;
   final MessageRole role;
   final DateTime timestamp;
@@ -11,7 +10,6 @@ class Message {
   Message({
     required this.id,
     required this.sessionId,
-    required this.userId,
     this.content,
     required this.role,
     required this.timestamp,
@@ -21,7 +19,6 @@ class Message {
     return {
       'id': id,
       'session_id': sessionId,
-      'user_id': userId,
       'content': content,
       'role': role.toString(),
       'timestamp': timestamp.toIso8601String(),
@@ -32,7 +29,6 @@ class Message {
     return Message(
       id: map['id'],
       sessionId: map['session_id'],
-      userId: map['user_id'],
       content: map['content'],
       role: MessageRole.values.firstWhere(
         (e) => e.toString() == map['role'],
@@ -45,7 +41,6 @@ class Message {
   Message copyWith({
     String? id,
     String? sessionId,
-    String? userId,
     String? content,
     MessageRole? role,
     DateTime? timestamp,
@@ -53,7 +48,6 @@ class Message {
     return Message(
       id: id ?? this.id,
       sessionId: sessionId ?? this.sessionId,
-      userId: userId ?? this.userId,
       content: content ?? this.content,
       role: role ?? this.role,
       timestamp: timestamp ?? this.timestamp,
