a853de09ee230ef1830a4eb0f40577054d6df6cb
zer0ken <a3592777@gmail.com>
2025-05-28 09:29:51 +0900

    ♻️ rename: AppDimens -> AppDimensions

--- lib/ui/constants/app_dimensions.dart
+++ lib/ui/constants/app_dimensions.dart
@@ -3,7 +3,7 @@ class AppDimens {
   static const double tabSize = 44.0;
   static const double tabSpacing = 10.0;
   static const double tabPadding = 4.0;
-  static const double containerRadius = 30.0;
   static const double tabRadiusLarge = 24.0;
   static const double tabRadiusSmall = 8.0;
+  static const double containerRadius = 30.0;
 }
