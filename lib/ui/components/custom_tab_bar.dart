abad48f0bd10fc45d57ed8ac274266b981b33cb9
zer0ken <a3592777@gmail.com>
2025-05-28 09:38:12 +0900

    ♻️ dimensions 분리

--- lib/ui/components/custom_tab_bar.dart
+++ lib/ui/components/custom_tab_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../constants/app_colors.dart';
-import '../constants/app_dimensions.dart';
+import '../constants/dimensions/tab_dimensions.dart';
+import '../constants/dimensions/container_dimensions.dart';
 
 class CustomTabBar extends StatelessWidget {
   final int selectedIndex;
@@ -15,23 +16,22 @@ class CustomTabBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-      padding: EdgeInsets.all(AppDimens.tabPadding),
+      margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),      padding: EdgeInsets.all(TabDimens.padding),
       decoration: BoxDecoration(
         color: AppColors.primary,
-        borderRadius: BorderRadius.circular(AppDimens.containerRadius),
+        borderRadius: BorderRadius.circular(ContainerDimens.radius),
       ),
       child: Stack(
         children: [
           AnimatedPositioned(
             duration: const Duration(milliseconds: 200),
             curve: Curves.easeInOut,
-            left: selectedIndex * (AppDimens.tabSize + AppDimens.tabSpacing),
+            left: selectedIndex * (TabDimens.size + TabDimens.spacing),
             child: AnimatedContainer(
               duration: const Duration(milliseconds: 200),
               curve: Curves.easeInOut,
-              width: AppDimens.tabSize,
-              height: AppDimens.tabSize,
+              width: TabDimens.size,
+              height: TabDimens.size,
               decoration: BoxDecoration(
                 color: AppColors.white,
                 borderRadius: _getBorderRadius(selectedIndex),
@@ -50,12 +50,11 @@ class CustomTabBar extends StatelessWidget {
   Widget _buildTab(int index) {
     return GestureDetector(
       onTap: () => onTap(index),
-      behavior: HitTestBehavior.opaque,
-      child: Padding(
-        padding: EdgeInsets.only(left: index > 0 ? AppDimens.tabSpacing : 0),
+      behavior: HitTestBehavior.opaque,      child: Padding(
+        padding: EdgeInsets.only(left: index > 0 ? TabDimens.spacing : 0),
         child: SizedBox(
-          width: AppDimens.tabSize,
-          height: AppDimens.tabSize,
+          width: TabDimens.size,
+          height: TabDimens.size,
           child: Center(
             child: AnimatedSwitcher(
               duration: const Duration(milliseconds: 200),
@@ -68,7 +67,7 @@ class CustomTabBar extends StatelessWidget {
                 color: selectedIndex == index
                     ? AppColors.primary
                     : AppColors.white,
-                size: AppDimens.tabIconSize,
+                size: TabDimens.iconSize,
               ),
             ),
           ),
@@ -76,20 +75,19 @@ class CustomTabBar extends StatelessWidget {
       ),
     );
   }
-
   BorderRadius _getBorderRadius(int index) {
     if (index == 0) {
-      return const BorderRadius.horizontal(
-        left: Radius.circular(AppDimens.tabRadiusLarge),
-        right: Radius.circular(AppDimens.tabRadiusSmall),
+      return BorderRadius.horizontal(
+        left: Radius.circular(TabDimens.radiusLarge),
+        right: Radius.circular(TabDimens.radiusSmall),
       );
     } else if (index == 2) {
-      return const BorderRadius.horizontal(
-        left: Radius.circular(AppDimens.tabRadiusSmall),
-        right: Radius.circular(AppDimens.tabRadiusLarge),
+      return BorderRadius.horizontal(
+        left: Radius.circular(TabDimens.radiusSmall),
+        right: Radius.circular(TabDimens.radiusLarge),
       );
     }
-    return BorderRadius.circular(AppDimens.tabRadiusSmall);
+    return BorderRadius.circular(TabDimens.radiusSmall);
   }
 
   IconData _getIcon(int index) {
---
+++ lib/ui/constants/dimensions/tab_dimensions.dart
@@ -0,0 +1,8 @@
+class TabDimens {
+  static const double iconSize = 24.0;
+  static const double size = 44.0;
+  static const double spacing = 10.0;
+  static const double padding = 4.0;
+  static const double radiusLarge = 24.0;
+  static const double radiusSmall = 8.0;
+}
