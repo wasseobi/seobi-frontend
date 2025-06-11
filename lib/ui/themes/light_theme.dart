33f503813a2ee24777ee581e10f1b53f92606d56
zer0ken <a37592777@gmail.com>
2025-06-11 09:44:26 +0900

    💄 CustomDrawer 색상 테마 적용

--- lib/ui/themes/light_theme.dart
+++ lib/ui/themes/light_theme.dart
@@ -5,23 +5,40 @@ class LightTheme {
   static ThemeData get theme => ThemeData(
     useMaterial3: true,
     brightness: Brightness.light,
+
     colorScheme: const ColorScheme.light(
       primary: AppColors.main100,
       onPrimary: AppColors.white100,
-      secondary: AppColors.gray100,
+      primaryContainer: AppColors.white100,
+      onPrimaryContainer: AppColors.black100,
+
+      secondary: AppColors.main80,
+      onSecondary: AppColors.white100,
+      secondaryContainer: AppColors.white80,
+      onSecondaryContainer: AppColors.black100,
+
       surface: Colors.white,
       onSurface: AppColors.black100,
+
       error: Colors.red,
     ),
+
     scaffoldBackgroundColor: Colors.white,
     appBarTheme: const AppBarTheme(
       backgroundColor: AppColors.main100,
       foregroundColor: AppColors.black100,
     ),
+
     tabBarTheme: const TabBarThemeData(
       labelColor: AppColors.black100,
       unselectedLabelColor: AppColors.white80,
       indicatorColor: AppColors.white100,
     ),
+
+    drawerTheme: const DrawerThemeData(
+      backgroundColor: AppColors.white100,
+    ),
+
+    dividerColor: AppColors.main80
   );
 }
